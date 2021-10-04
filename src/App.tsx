1e82490433dc354264e9a0c086d39d008aff17ff
kang-sw <ki6080@gmail.com>
2021-10-04 13:53:33 +0000

    Practicing

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Button } from "react-bootstrap"
+import React, { useState } from "react"
+import DockLayout, { LayoutData } from "rc-dock"
+import "rc-dock/dist/rc-dock.css"
+import Login from "./Login"
+
+let dockLayout: DockLayout;
+function getRef(r: DockLayout) {
+  dockLayout = r;
+}
+
+function SomeRenderer() {
+  const [str, setStr] = useState("hell, world!");
+  const addTab = () => {
+    const newTab = {
+      id: str,
+      content: <div>hello, my world!</div>,
+      title: str
+    };
+
+    dockLayout.dockMove(newTab, 'prime', 'middle')
+  }
+  return <Button onClick={() => { setStr(str + "dd"); addTab() }}>{str}</Button>
+}
+
+let defaultLayout: LayoutData = {
+  dockbox: {
+    mode: "horizontal",
+    children: [
+      {
+        id: 'prime',
+        tabs: [
+          { id: "tab1", title: "tabA", content: <SomeRenderer /> },
+          { id: "tab2", title: "tab2", content: <div>hellworld 2</div> }
+        ]
+      }
+    ]
+  }
+}
+
+function App() {
+  return (
+    <DockLayout
+      ref={getRef}
+      defaultLayout={defaultLayout}
+      style={{
+        position: "absolute",
+        left: 10,
+        top: 10,
+        right: 10,
+        bottom: 10
+      }}
+    />
+  );
+}
+
+export default App;
---
+++ src/Login.tsx
@@ -0,0 +1,12 @@
+import { InputGroup } from "react-bootstrap";
+import { useState } from "react";
+
+interface LoginProps {
+    setUrl: (url: String) => void;
+}
+
+function Login(prop: LoginProps) {
+ 
+}
+
+export default Login
